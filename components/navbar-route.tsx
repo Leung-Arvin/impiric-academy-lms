1a6a201b704ea5493469c6ae33415073e22610a6
Arvin-Leung <leungarvin@gmail.com>
2024-07-08 10:23:27 -0400

    implement category and title search queries for browse page

--- components/navbar-route.tsx
+++ components/navbar-route.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
     const pathname = usePathname();
@@ -12,27 +13,35 @@ export const NavbarRoutes = () => {
 
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayerPage = pathname?.startsWith("/chapter");
+    const isSearchPage = pathname === "/search"
 
     return (
-        <div className="flex gap-x-2 ml-auto">
-            {isTeacherPage || isPlayerPage ? (
-                <Link href="/">
-                <Button>  
-                    <LogOut className="h-4 w-4 mr-2"/>
-                    Exit
-                </Button>
-                </Link>
-            ): (
-                <Link href="/teacher/courses">
-                    <Button size="sm" variant="ghost">
-                        Teacher Mode
+        <>
+            {isSearchPage && (
+                <div className="hidden md:block">
+                    <SearchInput />
+                </div>
+            )}
+            <div className="flex gap-x-2 ml-auto">
+                {isTeacherPage || isPlayerPage ? (
+                    <Link href="/">
+                    <Button>  
+                        <LogOut className="h-4 w-4 mr-2"/>
+                        Exit
                     </Button>
-                </Link>
-            )} 
-            <UserButton
-                afterSignOutUrl="/"
-            />
+                    </Link>
+                ): (
+                    <Link href="/teacher/courses">
+                        <Button size="sm" variant="ghost">
+                            Teacher Mode
+                        </Button>
+                    </Link>
+                )} 
+                <UserButton
+                    afterSignOutUrl="/"
+                />
 
-        </div>
+            </div>
+        </>
     )
 }
